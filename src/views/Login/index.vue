4825e027835c91db4be6403e93070e19f3c6357f
Tantantant <499221986@qq.com>
2020-11-28 00:14:24 +0800

    feat:登录注册静态页面完成

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -5,8 +5,41 @@
         <div class="Login-login-Inner">
           <ul class="Login-login">
             <li>扫描登陆</li>
-            <li>账户登录</li>
+            <li class="clear-border">账户登录</li>
           </ul>
+          <div class="Login-content">
+            <form action="##">
+              <div class="Login-phone">
+                <i></i>
+                <input type="text" placeholder="手机号" />
+                <p class="Login-error-msg">错误提示信息</p>
+              </div>
+
+              <div class="Login-password">
+                <i class="pwd"></i>
+                <input type="text" placeholder="请输入密码" />
+                <p class="Login-error-msg">错误提示信息</p>
+              </div>
+
+              <div class="Login-Button">
+                <label class="checkbox inline">
+                  <input name="m1" type="checkbox" value="2" checked="" />
+                  自动登录
+                </label>
+                <span class="forget">忘记密码？</span>
+              </div>
+              <button class="Login-btn">登&nbsp;&nbsp;录</button>
+            </form>
+            <div class="Login-foot">
+                <ul>
+                  <li><img src="./images/qq.png" alt=""></li>
+                  <li><img src="./images/weixin.png" alt=""></li>
+                  <li><img src="./images/sina.png" alt=""></li>
+                  <li><img src="./images/ali.png" alt=""></li>
+                </ul>
+                <span>立即注册</span>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -21,7 +54,6 @@ export default {
 
 <style lang="less" scoped>
 .container {
-  
   height: 487px;
   background-color: #e93854;
 }
@@ -40,24 +72,111 @@ export default {
   padding: 20px;
   background-color: #fff;
 }
-.Login-login-Inner{
+.Login-login-Inner {
   width: 380px;
   height: 370px;
   border: 1px solid #ccc;
 }
-.Login-login{
+.Login-login {
   display: flex;
   width: 380px;
   height: 50px;
-  li{
+  border-bottom: 1px solid #ccc;
+  li {
     width: 190px;
     text-align: center;
     font-size: 20px;
     font-weight: bold;
     line-height: 50px;
-    :nth-child(1){
+    border-right: 1px solid #ccc;
+    :nth-child(1) {
       border-bottom: 1px solid #ccc;
     }
-  };
+  }
+  .clear-border {
+    border-right: none;
+  }
+}
+.Login-content {
+  width: 342px;
+  height: 186.6px;
+  padding: 20px;
+
+  .Login-password,
+  .Login-phone {
+    input {
+      width: 290px;
+      height: 33px;
+      border:1px solid #ccc;
+      display: flex;
+    }
+  }
+  .Login-phone i {
+    float: left;
+    width: 37px;
+    height: 35px;
+    border: 1px solid #ccc;
+    background: url("./images/icons.png") no-repeat -10px -201px;
+    box-sizing: border-box;
+    border-radius: 2px 0 0 2px;
+  }
+  .Login-password i {
+    float: left;
+    width: 37px;
+    height: 35px;
+    border: 1px solid #ccc;
+    background: url(./images/icons.png) no-repeat -10px -201px;
+    box-sizing: border-box;
+    border-radius: 2px 0 0 2px;
+  }
+  .Login-error-msg {
+    margin: 3px 40px 0;
+    color: red;
+  }
+  i {
+    float: left;
+    width: 37px;
+    height: 32px;
+    border: 1px solid;
+  }
+  .Login-Button {
+    display: flex;
+    justify-content: space-between;
+    margin-top: 10px;
+    .forget {
+      margin-right: 40px;
+    }
+  }
+}
+.Login-btn {
+  background-color: rgb(225, 37, 27);
+  font-size: 16px;
+  font-family: 微软雅黑;
+  word-spacing: 4px;
+  color: rgb(255, 255, 255);
+  width: 100%;
+  height: 36px;
+  margin-top: 25px;
+  padding: 6px;
+  border-radius: 0px;
+  border-width: 1px;
+  border-style: solid;
+  border-color: rgb(225, 37, 27);
+  border-image: initial;
+  outline: none;
+}
+.Login-foot{
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-top: 30px;
+  ul{
+    width:162px;
+    display: flex;
+    justify-content: space-between;
+  }
+  span{
+    font-size: 16px;
+  }
 }
 </style>
--- src/views/Register/index.vue
+++ src/views/Register/index.vue
@@ -1,15 +1,123 @@
 <template>
-  <div>
-      <h2>Register...</h2>
+  <div class="container">
+    <div class="Register-newUser">
+      <h3>
+        注册新用户
+        <span>我有账号，去<a href="###"> 登录</a></span>
+      </h3>
+    </div>
+    <div class="Register-content">
+      <form action="#">
+        <div class="Register-input">
+          <label>手机号:</label>
+          <input type="text" placeholder="请输入您的手机号" />
+          <p class="Register-error-msg">错误提示信息</p>
+        </div>
+        <div class="Register-input">
+          <label>验证码:</label>
+          <input type="text" placeholder="请输入验证码" />
+          <p class="Register-error-msg">错误提示信息</p>
+        </div>
+
+        <div class="Register-input">
+          <label>登录密码:</label>
+          <input type="text" placeholder="请输入您的登录密码" />
+          <p class="Register-error-msg">错误提示信息</p>
+        </div>
+        <div class="Register-input">
+          <label>确认密码:</label>
+          <input type="text" placeholder="请输入确认密码" />
+          <p class="Register-error-msg">错误提示信息</p>
+        </div>
+        <div class="Register-radio">
+          <input type="radio" />同意协议并注册《尚品汇用户协议》
+          <p class="Register-error-msg">错误提示信息</p>
+        </div>
+        <button class="Register-btn">完成注册</button>
+      </form>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-    name:"Register"
-}
+  name: "Register",
+};
 </script>
 
-<style>
-
+<style lang="less" scoped>
+.container {
+  width: 1200px;
+  height: 445px;
+  border: 1px solid #dfdfdf;
+  margin: 0 auto;
+}
+.Register-newUser {
+  h3 {
+    background: #ececec;
+    margin: 0;
+    padding: 6px 15px;
+    color: #333;
+    border-bottom: 1px solid #dfdfdf;
+    font-size: 20.04px;
+    line-height: 30.06px;
+    margin-bottom: 40px;
+    span {
+      font-size: 14px;
+      float: right;
+      a {
+        color: red;
+      }
+    }
+  }
+}
+.Register-content form {
+  position: relative;
+  width: 1200px;
+  margin: 0 auto;
+  .Register-input {
+    padding-left: 390px;
+    margin-bottom: 18px;
+    label {
+      font-size: 14px;
+      width: 96px;
+      text-align: right;
+      display: inline-block;
+    }
+    input {
+      width: 270px;
+      height: 38px;
+      padding-left: 8px;
+      box-sizing: border-box;
+      margin-left: 5px;
+      outline: none;
+      border: 1px solid #999;
+    }
+    p {
+      position: absolute;
+      left: 495px;
+      color: red;
+    }
+  }
+}
+.Register-radio {
+  text-align: center;
+  position: relative;
+  p{
+    position: relative;
+    left: -70px;
+    color: red;
+  }
+}
+.Register-btn {
+  outline: none;
+  width: 270px;
+  height: 36px;
+  background: #e1251b;
+  color: #fff !important;
+  display: inline-block;
+  font-size: 16px;
+  border: 1px solid #dfdfdf;
+  transform: translate(182%);
+}
 </style>
